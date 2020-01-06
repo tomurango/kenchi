14bd84e07a0f2257c01c00cb4f5c8467cfebb9b0
Eiji TOMURA <s1f101803758@iniad.org>
2020-01-06 14:31:01 +0900

    ver.0.6.2.8

--- public/js/subscription.js
+++ public/js/subscription.js
@@ -2,10 +2,12 @@
 //Payjp.setPublicKey("pk_test_9fd0175be5f918882aa96643");
 //payjpじゃなくて、ストライプのほうが連携機能が優れているのか？
 
-function onCreated(){
-    console.log("成功プラン");
+//正常な申請の時に呼び出される関数
+function onCreated(res){
+    console.log("成功プラン", res);
 }
 
-function onFailed(){
-    console.log("失敗しましたよプラン");
+//error の時に実行される関数
+function onFailed(res, error){
+    console.log("失敗しましたよプラン", res, error);
 }
